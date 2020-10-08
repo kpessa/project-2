656175bc9cf4d2e1bf1c16ccca49a0e6dd62cf21
Kurt Pessa <kpessa@gmail.com>
2020-10-08 07:03:41 -0400

    worked on chloropleth graph..

--- static/map/functions.js
+++ static/map/functions.js
@@ -11,3 +11,27 @@ const capitalize = (s) => {
   if (typeof s !== 'string') return ''
   return s.charAt(0).toUpperCase() + s.slice(1)
 }
+
+
+
+const getColor = (d,scale) => {
+  var colors = ['#a50026','#d73027','#f46d43','#fdae61','#fee090','#e0f3f8','#abd9e9','#74add1','#4575b4','#313695'].reverse()
+  for(var i=scale.length-1;i>=0;i--) {
+    if (d > scale[i]) {
+      return colors[i];
+    }
+  }
+}
+
+const createArr = max => {
+  var returnArr = []
+  for (var i=0; i<10;i++) {
+    returnArr.push(i*max/10)
+  }
+  return returnArr;
+}
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
--- static/map/map.js
+++ static/map/map.js
@@ -12,8 +12,82 @@ d3.json("/secret/API_KEY").then((api_json) => {
     Dark: dark,
   };
 
-  d3.json("static/map/counties.json").then((county_json) => {
-    let counties = L.geoJson(county_json)
+
+  d3.json("static/map/florida_data.geojson").then((florida_data) => {
+
+    function style(feature) {
+      return {
+        fillColor: getColor(feature.properties.fl_data_cases,createArr(10000)),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray:'3',
+        fillOpacity: 0.7
+      }
+    }
+
+    function highlightFeature(e) {
+      var layer = e.target;
+
+      layer.setStyle({
+        weight:5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+      })
+
+      if(!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+      }
+
+      info.update(layer.feature.properties)
+    }
+
+    function resetHighlight(e) {
+      counties.resetStyle(e.target)
+      info.update()
+    }
+
+    function zoomToFeature(e) {
+      mymap.fitBounds(e.target.getBounds())
+    }
+
+    function onEachFeature(feature,layer) {
+      layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+      })
+    }
+
+    var info = L.control();
+
+    info.onAdd = function(map) {
+      this._div = L.DomUtil.create('div','info');
+      this.update();
+      return this._div;
+    }
+
+    info.update = function (props) {
+      this._div.innerHTML =  props ? `<strong><h4>${props.NAMELSAD10}</h4><strong><hr>
+      <table>
+        <tr>
+          <td class="label">Population</td>
+          <td>${numberWithCommas((+props["fl_data_population"]).toFixed(0))}</td>
+        </tr>
+        <tr>
+          <td class="label">Cases</td>
+          <td>${numberWithCommas(props["fl_data_cases"])}</td>
+        </tr>
+      </table>` : 'Hover over a county..'
+    }
+    
+
+    let counties = L.geoJson(florida_data,{
+      style:style,
+      onEachFeature: onEachFeature
+    })
+
     var overlayMaps = { Counties: counties };
 
         var mymap = L.map("map", {
@@ -21,7 +95,7 @@ d3.json("/secret/API_KEY").then((api_json) => {
         }).setView([28, -83.5], 6);
 
         L.control.layers(baseMaps, overlayMaps).addTo(mymap);
-    
+        info.addTo(mymap)
     // d3.json("/api/v1.0/Florida_data").then((fl_data) => {
     //   d3.csv("static/map/fips.csv").then((fips) => {
     //   });
