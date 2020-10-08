dc1814552ee673be17e73ceb63ebd01f4631a96b
Kurt Pessa <kpessa@gmail.com>
2020-10-08 19:44:03 -0400

    udpated maps

--- static/map/functions.js
+++ static/map/functions.js
@@ -1,4 +1,4 @@
-makeLayer = (id, API_KEY) => {
+var makeLayer = (id, API_KEY) => {
   return L.tileLayer ("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
@@ -12,10 +12,10 @@ makeLayer = (id, API_KEY) => {
 //   return s.charAt(0).toUpperCase() + s.slice(1)
 // }
 
-getMax = (florida_data, variable) => d3.max(florida_data.features.map(feature => +feature.properties[variable]))
+var getMax = (florida_data, variable) => d3.max(florida_data.features.map(feature => +feature.properties[variable]))
   
 
-createArr = max => {
+var createArr = max => {
   var returnArr = []
   var buckets = 9;
   for (var i=0; i<buckets;i++) {
@@ -24,7 +24,94 @@ createArr = max => {
   return returnArr;
 }
 
-numberWithCommas = x => {
+var numberWithCommas = x => {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
+var getColor = (d,scale) => {
+  var colors = ['#a50026','#d73027','#f46d43','#fdae61','#fee090','#e0f3f8','#abd9e9','#74add1','#4575b4','#313695'].reverse()
+  for(var i=scale.length-1;i>=0;i--) {
+    if (d > scale[i]) {
+      return colors[i];
+    }
+  }
+}
+
+var highlightFeature = e => {
+  var layer = e.target;
+
+  layer.setStyle({
+    weight:5,
+    color: 'black',
+    dashArray: '',
+    fillOpacity: 0.7
+  })
+
+  if(!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+    layer.bringToFront();
+  }
+
+  info.update(layer.feature.properties)
+}
+
+var zoomToFeature = e => {
+  let county = e.target.feature.properties.NAME10
+  document.getElementById("county-dropdown").value = county
+  mymap.fitBounds(e.target.getBounds())
+}
+
+
+var info = L.control();
+
+info.onAdd = function(map) {
+  this._div = L.DomUtil.create('div','info');
+  this.update();
+  return this._div;
+}
+
+info.update = function (props) {
+  this._div.innerHTML =  props ? `<strong><h4>${props.NAMELSAD10}</h4><strong><hr>
+  <table>
+    <tr>
+      <td class="label">Case Rate*</td>
+      <td>${(+props["fl_data_case rate"]).toFixed(2)}</td>
+    </tr>
+    <tr>
+      <td class="label">Death Rate*</td>
+      <td>${(+props["fl_data_death rate"]).toFixed(4)}</td>
+    </tr>
+    <tr><td colspan="2"><p class="asterick">* per 100,000 population</p></td></tr>
+    <tr><td colspan="2"><hr></td></tr>
+    <tr>
+      <td class="label">Uninsured</td>
+      <td>${(+props["fl_data_uninsured percent"]).toFixed(0)}%</td>
+    </tr>
+    <tr>
+      <td class="label">Fair or Poor Health</td>
+      <td>${(+props["fl_data_fair or poor health percent"]).toFixed(0)}%</td>
+    </tr>
+  <tr><td colspan="2"><hr></td></tr>
+    <tr>
+      <td class="label">Median Income</td>
+      <td>$${numberWithCommas((+props["fl_data_median income 2018"]).toFixed(0))}</td>
+    </tr>
+    <tr>
+      <td class="label">Population</td>
+      <td>${+props["fl_data_population"]>0 ? numberWithCommas((+props["fl_data_population"]).toFixed(0)) : "missing data"}</td>
+    </tr>
+    <tr><td colspan="2"><hr></td></tr>
+    <tr>
+      <td class="label">Cases</td>
+      <td>${numberWithCommas(props["fl_data_cases"])}</td>
+    </tr>
+    
+    <tr>
+      <td class="label">Deaths</td>
+      <td>${numberWithCommas(props["fl_data_deaths"])}</td>
+    </tr>
+    
+  </table>
+  <hr>
+  
+  ` : 'Hover over a county..'
+}
--- static/map/map.js
+++ static/map/map.js
@@ -1,28 +1,33 @@
-var API_KEY = document.getElementById("key").innerText
+var API_KEY = document.getElementById("key").innerText;
+
+var mymap;
+(mymap == null) ? mymap = L.map("map").setView([28, -83.5], 7): console.log("running a second time");
 
 
-let satellite = makeLayer("satellite-v9", API_KEY);
-let streets = makeLayer("streets-v11", API_KEY);
-let light = makeLayer("light-v10", API_KEY);
-let dark = makeLayer("dark-v10", API_KEY);
+var satellite = makeLayer("satellite-v9", API_KEY);
+var streets = makeLayer("streets-v11", API_KEY);
+var light = makeLayer("light-v10", API_KEY);
+var dark = makeLayer("dark-v10", API_KEY);
 
-let baseMaps = {
+var baseMaps = {
   Satellite: satellite,
   Streets: streets,
   Light: light,
   Dark: dark,
 };
 
+light.addTo(mymap);
+
 d3.json("static/map/florida_data.geojson").then((florida_data) => {
   
-  let cases = createOverlay(florida_data, "fl_data_cases");
-  let case_rate = createOverlay(florida_data, "fl_data_case rate")
-  let median_income = createOverlay(florida_data, "fl_data_median income 2018")
-  let population = createOverlay(florida_data, "fl_data_population")
-  let deaths = createOverlay(florida_data,"fl_data_deaths")
-  let death_rate = createOverlay(florida_data, "fl_data_death rate")
-  let uninsured_perc = createOverlay(florida_data, "fl_data_uninsured percent")
-  let poor_health = createOverlay(florida_data, "fl_data_fair or poor health percent")
+  var cases = createOverlay(florida_data, "fl_data_cases");
+  var case_rate = createOverlay(florida_data, "fl_data_case rate")
+  var median_income = createOverlay(florida_data, "fl_data_median income 2018")
+  var population = createOverlay(florida_data, "fl_data_population")
+  var deaths = createOverlay(florida_data,"fl_data_deaths")
+  var death_rate = createOverlay(florida_data, "fl_data_death rate")
+  var uninsured_perc = createOverlay(florida_data, "fl_data_uninsured percent")
+  var poor_health = createOverlay(florida_data, "fl_data_fair or poor health percent")
 
   var overlayMaps = { 
     "Uninsured %": uninsured_perc,
@@ -35,9 +40,7 @@ d3.json("static/map/florida_data.geojson").then((florida_data) => {
     '<img src="static/images/deaths.png"><span> Death Rate</span>': death_rate
   };
 
-  let mymap = L.map("map", {
-    layers: [light, case_rate],
-  }).setView([28, -83.5], 7);
+  case_rate.addTo(mymap);
 
   L.control.layers(baseMaps, overlayMaps).addTo(mymap);
   info.addTo(mymap)
