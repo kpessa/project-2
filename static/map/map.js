3e8784a3b7eda8861c0fa9f4fea3879bbe5d82c2
szerp <szerp002@gmail.com>
2020-10-08 20:50:41 -0400

    updating scatter data origin to heroku path

--- static/map/map.js
+++ static/map/map.js
@@ -1,49 +1,49 @@
 
-d3.json("/secret/API_KEY").then((api_json) => {
-  let API_KEY = api_json.API_KEY;
-  let satellite = makeLayer("satellite-v9", API_KEY);
-  let streets = makeLayer("streets-v11", API_KEY);
-  let light = makeLayer("light-v10", API_KEY);
-  let dark = makeLayer("dark-v10", API_KEY);
+// d3.json("/secret/API_KEY").then((api_json) => {
+//   let API_KEY = api_json.API_KEY;
+//   let satellite = makeLayer("satellite-v9", API_KEY);
+//   let streets = makeLayer("streets-v11", API_KEY);
+//   let light = makeLayer("light-v10", API_KEY);
+//   let dark = makeLayer("dark-v10", API_KEY);
 
-  let baseMaps = {
-    Satellite: satellite,
-    Streets: streets,
-    Light: light,
-    Dark: dark,
-  };
+//   let baseMaps = {
+//     Satellite: satellite,
+//     Streets: streets,
+//     Light: light,
+//     Dark: dark,
+//   };
 
 
-  d3.json("static/map/florida_data.geojson").then((florida_data) => {
+//   d3.json("static/map/florida_data.geojson").then((florida_data) => {
     
-    let cases = createOverlay(florida_data, "fl_data_cases");
-    let case_rate = createOverlay(florida_data, "fl_data_case rate")
-    let median_income = createOverlay(florida_data, "fl_data_median income 2018")
-    let population = createOverlay(florida_data, "fl_data_population")
-    let deaths = createOverlay(florida_data,"fl_data_deaths")
-    let death_rate = createOverlay(florida_data, "fl_data_death rate")
-    let uninsured_perc = createOverlay(florida_data, "fl_data_uninsured percent")
-    let poor_health = createOverlay(florida_data, "fl_data_fair or poor health percent")
+//     let cases = createOverlay(florida_data, "fl_data_cases");
+//     let case_rate = createOverlay(florida_data, "fl_data_case rate")
+//     let median_income = createOverlay(florida_data, "fl_data_median income 2018")
+//     let population = createOverlay(florida_data, "fl_data_population")
+//     let deaths = createOverlay(florida_data,"fl_data_deaths")
+//     let death_rate = createOverlay(florida_data, "fl_data_death rate")
+//     let uninsured_perc = createOverlay(florida_data, "fl_data_uninsured percent")
+//     let poor_health = createOverlay(florida_data, "fl_data_fair or poor health percent")
 
 
-    var overlayMaps = { 
-      "Uninsured %": uninsured_perc,
-      "Fair or Poor Health %": poor_health,
-      "Median Income": median_income,
-      "Population": population,
-      "Cases": cases,
-      "Case Rate": case_rate,
-      "Deaths": deaths,
-      "Death Rate": death_rate
+//     var overlayMaps = { 
+//       "Uninsured %": uninsured_perc,
+//       "Fair or Poor Health %": poor_health,
+//       "Median Income": median_income,
+//       "Population": population,
+//       "Cases": cases,
+//       "Case Rate": case_rate,
+//       "Deaths": deaths,
+//       "Death Rate": death_rate
       
-    };
+//     };
 
-        var mymap = L.map("map", {
-          layers: [light, case_rate],
-        }).setView([28, -83.5], 7);
+//         var mymap = L.map("map", {
+//           layers: [light, case_rate],
+//         }).setView([28, -83.5], 7);
 
-        L.control.layers(baseMaps, overlayMaps).addTo(mymap);
-        info.addTo(mymap)
+//         L.control.layers(baseMaps, overlayMaps).addTo(mymap);
+//         info.addTo(mymap)
     
-  });
-});
+//   });
+// });
--- static/scatter/scatter.js
+++ static/scatter/scatter.js
@@ -1,4 +1,5 @@
 // Define SVG area dimensions
+
 var svgWidth = parseInt(d3.select('#d3-scatter').style('width'));
 var svgHeight = 400;
 var hHeight = parseInt(d3.select('#scatter-title-height').style('height'));
@@ -6,16 +7,16 @@ var hHeight = parseInt(d3.select('#scatter-title-height').style('height'));
 // console.log(svgWidth, svgHeight);
 // svgWidth - (svgWidth/3.9);
 
-var margin = {t:10, r:30, b:80, l:100} 
+var margin = { t: 10, r: 30, b: 80, l: 100 }
 // svgWidth/100;
 // var pad = svgWidth/5;
 // var labelArea = svgWidth/5;
 
 // Define dimensions of the chart area
-var chartWidth = svgWidth-margin.l-margin.r;
+var chartWidth = svgWidth - margin.l - margin.r;
 // svgWidth - (margin * 20);
 // 370 
-var chartHeight = svgHeight - hHeight-margin.t-margin.b;
+var chartHeight = svgHeight - hHeight - margin.t - margin.b;
 // 300
 // svgHeight - (margin * 5);
 
@@ -27,34 +28,51 @@ var svg = d3.select("#d3-scatter")
     .call(responsivefy)
 
 var chart = svg.append('g')
-    .attr("transform",`translate(${margin.l} ${margin.t})`)
+    .attr("transform", `translate(${margin.l} ${margin.t})`)
     .attr("width", chartWidth)
     .attr("height", chartHeight)
 
-// // Load data 
-// var queryUrl = "/api/v1.0/Florida_data";
+// Relative path for data origin 
+var queryUrl = "/api/v1.0/Florida_data";
 
-// Perform a GET request to the query URL
-// d3.json(queryUrl, function(data) {
+//  CSV Testing
+// var queryUrl = "static/data/fl_data.csv";
 
-var data_url = "static/data/fl_data.csv";
-scatter_plot(data_url)
+// Loads Scatter
+scatter_plot(queryUrl)
 
-function scatter_plot(data_origin){
+// Function that populates the scatter plot
+function scatter_plot(data_origin) {
 
-
-    d3.csv(data_origin).then(function(data) {
+    // Perform a GET request to the query URL
+    d3.json(data_origin).then(function(data) {
+        // console.log('here i am');
+        // d3.csv(data_origin).then(function(data) {
         // console.log(data);
-        
-        var x = 'median income 2018';
+
+        var x = 'median income (2018)';
         var y = 'death rate';
 
         // X and Y Axis Max/Min Values
-        var xValMax = d3.max(data.map(d => d[x]))*1.10;
-        var xValMin = d3.min(data.map(d => d[x]))*0.90;
-        var yValMax = d3.max(data.map(d => parseFloat(d[y])))*1.10;
-        var yValMin = d3.min(data.map(d => parseFloat(d[y])))*0.90;
+        var xVals = Object.values(data).map(d => d[x]);
+        var yVals = Object.values(data).map(d => d[y]);
+
+
+        console.log((typeof( yVals)));
         
+        // var test = Object.values(yVals);
+        // console.log(test);
+
+
+        // Min and Max x and y axis values
+        var xValMax = Math.max.apply(Math, Object.values(xVals))* 1.10;
+        var xValMin = Math.min.apply(Math, Object.values(xVals))* 0.90;
+        var yValMax = Math.max.apply(Math, Object.values(yVals))* 1.10;
+        var yValMin = Math.min.apply(Math, Object.values(yVals))* 0.90;
+        
+        console.log(xValMax, xValMin, yValMax, yValMin);
+
+
         // Add X axis
         var xScale = d3.scaleLinear()
             .domain([xValMin, xValMax])
@@ -71,21 +89,24 @@ function scatter_plot(data_origin){
             .data(data)
             .enter()
             .append("circle")
-            .attr("cx", function (d) { return xScale(d[x]) ; } )
-            .attr("cy", function (d) { return yScale(d[y]); } )
+            .attr("cx", function(d) { return xScale(Object.values(data).map(d => d[x])); })
+            .attr("cy", function(d) { return yScale(Object.values(data).map(d => d[y])); })
+
+            // .attr("cx", function (d) { return xScale(Object.values(d[x])); })
+            // .attr("cy", function (d) { return yScale(Object.values(d[y])); })
             .attr("r", 5)
-            .classed('bubbles',true);
-            // .attr("class", "stateCircle");
-        
-            console.log(svg.selectAll("text"));
-            
-            // Add Text
+            .classed('bubbles', true);
+        // .attr("class", "stateCircle");
+
+        // console.log(svg.selectAll("text"));
+
+        // Add Text
         var text = chart.selectAll("text")
-                    .data(data)
-                    .enter()
-                    .append("text");
-        
-                    
+            .data(data)
+            .enter()
+            .append("text");
+
+
         // var textLabels = text
         //                 .attr("x", function (d) { return  xScale(d[x]); } )
         //                 .attr("y", function (d) { return yScale(d[y] -.3); } )
@@ -97,55 +118,58 @@ function scatter_plot(data_origin){
         var yAxis = d3.axisLeft(yScale);
         var xAxis = d3.axisBottom(xScale)
             .ticks(4)
-            .tickFormat(d=> '$'+d3.format(',')(d))
+            .tickFormat(d => '$' + d3.format(',')(d))
 
         // Set x to the bottom of the chart
         chart.append("g")
             .attr("transform", `translate(0, ${chartHeight})`)
             .call(xAxis)
-            .attr('class','xAxis')
+            .attr('class', 'xAxis')
         // Testing calc
         // group margin and label area translation - shift down by chart height vs svg height
-        
+
 
         // set y to the y axis
         chart.append("g")
             .call(yAxis)
-            .attr('class','yAxis')
+            .attr('class', 'yAxis')
         // Testing calc
         // .attr('transform', `translate(${margin + labelArea}, 0)`);
 
         // Y axis label
-        var axisLabelX = chartWidth/10;
+        var axisLabelX = chartWidth / 10;
         var axisLabelY = chartHeight / 2.5;
-        
+
         svg.append('g')
-            .attr("transform", `translate(${margin.l / 2-15} ${(svgHeight-margin.b)/2})`)
+            .attr("transform", `translate(${margin.l / 2 - 15} ${(svgHeight - margin.b) / 2})`)
             .append('text')
             // .attr("dy", "1em")
             .attr('text-anchor', 'middle')
             .attr('transform', 'rotate(-90)')
             .text(y)
-            .classed('axis',true)
-            
-            
-            // .style('font-weight', 'bold');
-        
-        // X axis label
-        var axisLabelX = chartHeight/100;
-        var axisLabelY = chartWidth / 2;
-        
-        svg.append("text")
-            // .attr("text-anchor", "end")
-            .attr("x", (svgWidth- (svgWidth-chartWidth))/2+20)
-            .attr("y", svgHeight - margin.b/2-10)
-            .text(x)
-            .classed('axis',true)
+            .classed('axis', true)
+
+
         // .style('font-weight', 'bold');
 
-  
+        // X axis label
+        var axisLabelX = chartHeight / 100;
+        var axisLabelY = chartWidth / 2;
 
-}).catch(function(error) {
-        // console.log(error);
-});
+        svg.append("text")
+            // .attr("text-anchor", "end")
+            .attr("x", (svgWidth - (svgWidth - chartWidth)) / 2 + 20)
+            .attr("y", svgHeight - margin.b / 2 - 10)
+            .text(x)
+            .classed('axis', true)
+        // .style('font-weight', 'bold');
+
+
+    });
 };
+
+// })
+// .catch(function(error) {
+//         // console.log(error);
+// });
+// };
